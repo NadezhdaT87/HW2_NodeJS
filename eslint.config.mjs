4ce2620220473359e235c1b8df59e784274beb11
NadezhdaT <kybalibri@mail.ru>
2024-10-07 15:42:35 +0700

    change eslint and pretteir

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,35 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
-
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintComfigPrettier from "eslint-config-prettier";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
   {
-    "plugins": ["prettier"]
+    plugins: {
+      prettier: prettierPlugin
+    }
   },
-  {
-  "rules": {
-    "no-var": "error",
-    "semi": "error",
-    "indent": "error",
-    "no-multi-spaces": "error",
-    "space-in-parens": "error",
-    "no-multiple-empty-lines": "error",
-    "prefer-const": "error",
-    "no-use-before-define": "error"
+  pluginJs.configs.recommended,  
+  { 
+    languageOptions: {
+      globals: {
+        ...globals.browser        
+      }
+    }
+  },  
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"],    
+    rules: {
+      ...eslintComfigPrettier.rules,
+      "prettier/prettier": "error",      
+      "no-var": "error",
+      "semi": "error",
+      "indent": "error",
+      "no-multi-spaces": "error",
+      "space-in-parens": "error",
+      "no-multiple-empty-lines": "error",
+      "prefer-const": "error",
+      "no-use-before-define": "error",      
     }
   }
 ];
